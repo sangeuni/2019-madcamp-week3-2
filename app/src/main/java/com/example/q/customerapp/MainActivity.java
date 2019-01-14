417c6e15fba06aea358e384c79c0fc11c9ee6ee2
sangeuni <goakgoak1995@gmail.com>
2019-01-15 01:03:50 +0900

    first commit

---
+++ app/src/main/java/com/example/q/customerapp/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.q.customerapp;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initializing the TabLayout;
+        tabLayout = (TabLayout)findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText("QR SCAN"));
+        tabLayout.addTab(tabLayout.newTab().setText("MY PAGE"));
+        tabLayout.addTab(tabLayout.newTab().setText("MENU"));
+        tabLayout.addTab(tabLayout.newTab().setText("REVIEW"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        //Initializing ViewPager
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+
+        //Creating adapter
+        TabPagerAdapter pagerAdapter = new TabPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        //Set TabSelectedListener
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+    }
---
+++ app/src/main/java/com/example/q/customerapp/TabPagerAdapter.java
@@ -0,0 +1,44 @@
+package com.example.q.customerapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class TabPagerAdapter extends FragmentStatePagerAdapter {
+
+    //Count number of tabs
+    private int tabCount;
+
+    public TabPagerAdapter(FragmentManager fm, int tabCount) {
+        super(fm);
+        this.tabCount = tabCount;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        //Returning the current tabs
+        switch (position){
+            case 0:
+                MainTabFragment1 mainTabFragment1 = new MainTabFragment1();
+                return mainTabFragment1;
+            case 1:
+                MainTabFragment2 mainTabFragment2 = new MainTabFragment2();
+                return mainTabFragment2;
+            case 2:
+                MainTabFragment3 mainTabFragment3 = new MainTabFragment3();
+                return mainTabFragment3;
+            case 3:
+                MainTabFragment4 mainTabFragment4 = new MainTabFragment4();
+                return mainTabFragment4;
+
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}

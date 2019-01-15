92e15ba9f918ea892c453d958c856f2d30b9420a
wldus7660 <wldus7660@naver.com>
2019-01-15 14:49:13 +0900

    update

--- app/src/main/java/com/example/q/customerapp/MainTabFragment3.java
+++ app/src/main/java/com/example/q/customerapp/MainTabFragment3.java
@@ -11,8 +11,7 @@ public class MainTabFragment3 extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.tab_fragment3, container, false);
     }
